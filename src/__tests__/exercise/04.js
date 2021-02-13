bac225de49446f618f70d42e9bc02349cdb00441
juanb86 <juan.e.begnis@gmail.com>
2021-02-12 22:27:24 -0300

    04-extra4

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,23 +5,20 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
-import faker from 'faker'
+import {build, fake} from '@jackfranklin/test-data-bot'
 
-function buildLoginForm(overrides) {
-  return {
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-    ...overrides,
-  }
-}
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
-  // let submittedData = ''
-  // const handleSubmit = data => (submittedData = data)
   const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
-  const {username, password} = buildLoginForm({password: 'nolase'})
-
+  const {username, password} = buildLoginForm()
+  
   const usernameField = screen.getByRole('textbox', {
     name: /username/i,
   })
